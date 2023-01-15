da8993d8e7e8ba78e36e6be25b9b9e7649d4815c
kinsei0916 <kinsei0916@gmail.com>
2023-01-15 11:46:08 +0900

    Factor out authorization flow

---
+++ esap/auth.py
@@ -0,0 +1,70 @@
+import getpass
+from typing import Union
+
+import httplib2
+
+from esap import errors
+from esap import oauth2
+from esap import storage
+
+ENDPOINT_BASE = 'https://api.esa.io/'
+OOB_CALLBACK_URN = 'urn:ietf:wg:oauth:2.0:oob'
+
+
+def _client_secrets_storage_factory():
+  return storage.LocalFileStorage('~/.esap/client_secrets', secure=True)
+
+
+def _credentials_storage_factory():
+  return storage.LocalFileStorage('~/.esap/credentials', secure=True)
+
+
+class Auth(object):
+
+  def __init__(self):
+    self.client = oauth2.OAuth2Client(_client_secrets_storage_factory(),
+                                      _credentials_storage_factory(),
+                                      scope=['read', 'write'])
+
+  def authorize(self):
+    if self.client.access_token is None:
+      self._get_authorization_code()
+      self._fetch_access_token()
+
+  def add_token(
+      self,
+      uri: str,
+      method: str,
+      body: Union[str, None] = None,
+      headers: Union[dict[str, str], None] = None
+  ) -> tuple[str, dict[str, str], Union[str, None]]:
+    return self.client.add_token(uri,
+                                 http_method=method,
+                                 body=body,
+                                 headers=headers)
+
+  def _build_uri(self, endpoint: str):
+    return ENDPOINT_BASE + endpoint
+
+  def _get_authorization_code(self):
+    uri, _, _ = self.client.prepare_authorization_request(
+        self._build_uri('oauth/authorize'), redirect_url=OOB_CALLBACK_URN)
+    print('Please open the following URL in your browser:')
+    print()
+    print(f'    {uri}')
+    print()
+    code = getpass.getpass('Enter the code: ').strip()
+    self.client.set_code(code)
+
+  def _send_auth_request(self, uri: str, headers=None, body=None):
+    http = httplib2.Http()
+    response, content = http.request(uri, 'POST', body=body, headers=headers)
+    if response.status >= 300:
+      raise errors.HttpError(response, content, uri=uri)
+    return content
+
+  def _fetch_access_token(self):
+    uri, headers, body = self.client.prepare_token_request(
+        self._build_uri('oauth/token'))
+    resp_content = self._send_auth_request(uri, headers, body)
+    self.client.parse_request_body_response(resp_content)
--- esap/client.py
+++ esap/client.py
@@ -1,77 +1,26 @@
-import getpass
 import json
 from typing import Union
 import urllib.parse
 
 import httplib2
 
+from esap import auth
 from esap import errors
-from esap import oauth2
-from esap import storage
 
 ENDPOINT_BASE = 'https://api.esa.io/'
-OOB_CALLBACK_URN = 'urn:ietf:wg:oauth:2.0:oob'
-
-
-def _client_secrets_storage_factory():
-  return storage.LocalFileStorage('~/.esap/client_secrets', secure=True)
-
-
-def _credentials_storage_factory():
-  return storage.LocalFileStorage('~/.esap/credentials', secure=True)
 
 
 class EsaClient(object):
 
   def __init__(self):
-    self.oauth_client = oauth2.OAuth2Client(_client_secrets_storage_factory(),
-                                            _credentials_storage_factory(),
-                                            scope=['read', 'write'])
-    if not self._validate_access_token():
-      self._authorize()
-      self._fetch_access_token()
+    self.auth = auth.Auth()
+    self.auth.authorize()
 
   def _build_uri(self, endpoint: str, query_params: Union[dict, None] = None):
     uri = ENDPOINT_BASE + endpoint
     if query_params:
       uri += '?' + urllib.parse.urlencode(query_params)
     return uri
-
-  def _build_authorize_uri(self):
-    uri, _, _ = self.oauth_client.prepare_authorization_request(
-        self._build_uri('oauth/authorize'), redirect_url=OOB_CALLBACK_URN)
-    return uri
-
-  def _authorize(self):
-    authorize_uri = self._build_authorize_uri()
-    print('Please open the following URL in your browser:')
-    print()
-    print(f'    {authorize_uri}')
-    print()
-    code = getpass.getpass('Enter the code: ').strip()
-    self.oauth_client.set_code(code)
-
-  def _send_auth_request(self, uri: str, headers=None, body=None):
-    http = httplib2.Http()
-    response, content = http.request(uri, 'POST', body=body, headers=headers)
-    if response.status >= 300:
-      raise errors.HttpError(response, content, uri=uri)
-    return content
-
-  def _fetch_access_token(self):
-    uri, headers, body = self.oauth_client.prepare_token_request(
-        self._build_uri('oauth/token'))
-    resp_content = self._send_auth_request(uri, headers, body)
-    self.oauth_client.parse_request_body_response(resp_content)
-
-  def _validate_access_token(self):
-    if self.oauth_client.access_token is None:
-      return False
-    try:
-      self.get_request('oauth/token/info')
-      return True
-    except errors.HttpError:
-      return False
 
   def get_request(self, endpoint: str, query_params=None, headers=None):
     return self._send_request(endpoint,
@@ -97,10 +46,10 @@ class EsaClient(object):
       headers = {}
     headers.update({'Content-Type': 'application/json'})
 
-    uri, headers, body = self.oauth_client.add_token(uri,
-                                                     http_method=method,
-                                                     body=body,
-                                                     headers=headers)
+    uri, headers, body = self.auth.add_token(uri,
+                                             method=method,
+                                             body=body,
+                                             headers=headers)
 
     http = httplib2.Http()
     response, content = http.request(
