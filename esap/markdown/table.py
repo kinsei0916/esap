9cf459984eb041b40e3379d0c2ffcab9acf89b0e
kon72 <kinsei0916@gmail.com>
2023-02-11 20:06:38 +0900

    Update minify_markdown_table to limit the length of divider

--- esap/markdown/table.py
+++ esap/markdown/table.py
@@ -1,12 +1,17 @@
 import re
 
+_MAX_DIVIDER_LENGTH = 8
+
 
 def minify_markdown_table(md: str) -> str:
+  # Remove extra spaces around cells
   md = re.sub(r'\| *', '| ', md)
-  md = re.sub(r' *\|', ' |', md)
   md = re.sub(r' *\|', ' |', md)
   lines = md.splitlines()
   lines = [line.strip() for line in lines]
-  lines[1] = re.sub(r'-+', '--------', lines[1])
+  if len(lines) > 1:
+    # Limit the length of divider
+    pattern = fr'-{{{_MAX_DIVIDER_LENGTH},}}'
+    lines[1] = re.sub(pattern, '-' * _MAX_DIVIDER_LENGTH, lines[1])
   md = '\n'.join(lines)
   return md
